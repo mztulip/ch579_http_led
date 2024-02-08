94ba3e79661d1e559c4c9f27d7e2fc15760a5f05
Mateusz <mateusz@tulip.lol>
2024-02-08 19:59:52 +0100

    low leve output first idea to write it correctly

--- Net/src/ethernetif.c
+++ Net/src/ethernetif.c
@@ -133,16 +133,31 @@ static err_t low_level_output(struct netif *netif, struct pbuf *p)
 {
   struct   pbuf *q;
   uint8_t  snd_status = 0;
- 
+  printf("\tlow_level_output\n\r");
+  //sprawdz czy wyslane
+  //kopiuj do bufora
+  //wyslij
+  pbuf_copy_partial(p, mac_send_buffer, p->tot_len, 0);
+
+
   for(q = p; q != NULL; q = q->next) 
   {
+    printf("qbuf:\n\r %x:", (uint32_t)q);
     /* Send the data from the pbuf to the interface, one pbuf at a
        time. The size of the data in each pbuf is kept in the ->len
        variable. */
+      printf("Data len:%d Addr:%d\n\r", q->len, (uint32_t)q->payload);
+      for(int i = 0 ; i < q->len;i++)
+      {
+        printf("%c", *(uint8_t*)(q->payload+i));
+      }
+      // printf("\n\rEnd Data:\n\r");
+    // print_eth2_frame(q->payload, q->len);
 
 		//printf("low q->len:%d\r\n", q->len);
 		snd_status = ETHSendX(q->payload, q->len);
   }
+  printf("\n\r\tlow_level_output_end\n\r");
 
   return  (err_t)snd_status;
 }
